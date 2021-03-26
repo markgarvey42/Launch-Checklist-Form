baf995db39b7ad36709c1ba06faee24753d88953
Mark <Markgarvey42@gmail.com>
2021-03-25 20:29:37 -0500

    Working on bonus

--- script.js
+++ script.js
@@ -127,25 +127,29 @@ window.addEventListener("load", function() {
             
             document.getElementById("launchStatus").innerHTML = "Shuttle Is Ready for Launch"
             document.getElementById("launchStatus").style.color = 'green';
+
+
    
             let json = [];
             fetch("https://handlers.education.launchcode.org/static/planets.json")
             .then(response => response.json())
             .then(function(json) {
                const missionTarget = document.getElementById("missionTarget");
+               let index = json.length;
+
                missionTarget.innerHTML = `
                <h2>Mission Destination</h2>
                   <ol>
-                     <li>Name: ${json[0].name}</li>
-                     <li>Diameter: ${json[0].diameter}</li>
-                     <li>Star: ${json[0].star}</li>
-                     <li>Distance from Earth: ${json[0].distance}</li>
-                     <li>Number of Moons: ${json[0].moons}</li>
+                     <li id="destinationName">Name: ${json[0].name}</li>
+                     <li id="destinationDiameter">Diameter: ${json[0].diameter}</li>
+                     <li id="destinationStar">Star: ${json[0].star}</li>
+                     <li id="destinationDistance">Distance from Earth: ${json[0].distance}</li>
+                     <li id="destinationMoons">Number of Moons: ${json[0].moons}</li>
                   </ol>
-               <img src="${json[0].image}">
+               <img id="destinationPicture" src="${json[0].image}">
                `;
+
             });
-   
          };
       }
 
